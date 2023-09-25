fefb3484c41e37c71e43f5d1345c00a68bcdd994
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-25 11:43:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+const record =  [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "W"},
+    { year: "2010", result: "N/A"}
+]
+
+function superbowlWin(record) {
+    let result = record.find(record => record.result === "W")
+    return !!result ? result.year : undefined
+}
